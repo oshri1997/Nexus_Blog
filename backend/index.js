1b83fd9bd7b5adee994078232f86a1bff488a391
oshri1997 <oshri19970@gmail.com>
2024-03-16 19:18:45 +0200

    create-errorhandler function in utils file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./utils.js";
+import { connectDB, errorHandler } from "./utils.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
@@ -10,6 +10,7 @@ connectDB();
 const port = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} `);
