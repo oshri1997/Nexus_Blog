6f0d1365f06205e4bee5a981a5cf67a9786fff5b
oshri1997 <oshri19970@gmail.com>
2024-03-25 22:30:02 +0200

    add redux toolkit and redux persist for sign in

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,26 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import userReducer from "./user/userSlice";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }), //to avoid error with redux-persist
+});
+
+export const persistor = persistStore(store);
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
